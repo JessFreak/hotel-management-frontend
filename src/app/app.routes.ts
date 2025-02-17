69412d970ccd2bb9f13dd7e34320ca7510745da9
JessFreak <maliy.olexander@lll.kpi.ua>
2025-02-17 18:49:09 +0200

    added basic components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.component.html',
+  standalone: true,
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {}
