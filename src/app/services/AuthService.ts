52e72c2cd407f41a09512aa2e34a2178e3c5c638
JessFreak <maliy.olexander@lll.kpi.ua>
2025-02-19 14:08:42 +0200

    added environment file

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private baseUrl = 'http://localhost:3000/auth';
+  private readonly baseUrl = `${environment.apiUrl}/auth`;
 
   register(registerForm: any) {
     console.log(registerForm);
